a024d5d2f3dea778354dbd6f52220152d371eb77
syahrulutomo <irullvandalis@gmail.com>
2019-08-25 07:53:15 +0700

    profil dokter complete

--- src/components/ProfilDokter.js
+++ src/components/ProfilDokter.js
@@ -3,6 +3,7 @@ import Grey from './../img/grey.jpg';
 import ArrowRight from './../img/arrow-right.png';
 import ArrowLeft from './../img/arrow left.png';
 import Share from './../img/share.png';
+import Lokasi from './../img/lokasi.png';
 import { connect } from 'react-redux';
 import { fetchingDokterById } from './../actions/getDokterById';
 import { fetchingKlinik } from '../actions/getKlinik';
@@ -14,9 +15,15 @@ export class ProfilDokter extends Component {
         this.state = {
             showAllKeahlian: false,
             showAllPenyakit: false,
+            showAllJadwal: false,
+            showAllLokasi: false,
+            jadwalPraktik: [],
+            lokasiPraktik: [],
         }
+        this.showAllJadwal = this.showAllJadwal.bind(this)
         this.showAllKeahlian = this.showAllKeahlian.bind(this);
         this.showAllPenyakit = this.showAllPenyakit.bind(this);
+        this.showAllLokasi = this.showAllLokasi.bind(this);
         this.getTempatPraktik = this.getTempatPraktik.bind(this);
     }
 
@@ -25,7 +32,30 @@ export class ProfilDokter extends Component {
     }
 
     getTempatPraktik(){
-        
+        let jadwalList = [];
+        for(let i = 0; i < this.props.klinik.length; i++){    
+            for(let j = 0; j < this.props.klinik[i]['jadwal'].length; j++){
+                if(this.props.selectedDokter.id !== undefined){
+                    if(this.props.klinik[i]['jadwal'][j]['id_dokter'] == this.props.selectedDokterId){
+                        const temp = this.props.klinik[i];
+                        jadwalList.push([temp,this.props.klinik[i]['jadwal'][j]]);
+                    }
+                } 
+            }
+        }
+        return jadwalList;
+    }
+
+    showAllJadwal(){
+        this.setState({
+            showAllJadwal: !this.state.showAllJadwal
+        })
+    }
+
+    showAllLokasi(){
+        this.setState({
+            showAllLokasi: !this.state.showAllLokasi
+        })
     }
 
     showAllKeahlian(){
@@ -41,10 +71,129 @@ export class ProfilDokter extends Component {
     }
 
     render() {
-        console.log(this.props.selectedDokterId);
-        console.log(this.props.selectedDokter);
-        console.log(this.props.klinik);
+        console.log(this.getTempatPraktik());
+        const days = ["Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
+        const month = ["Januari", "Februari", "Maret", "April", "Mei", "Juni",
+                            "Juli", "Agustus", "September", "October", "November", "Desember"];
+        const monthDays = [31, 28, 31, 30, 31, 30,
+                            31, 31, 30, 31, 30, 31];
+        const d = new Date();
+        
+        const lokasiList = (this.getTempatPraktik().length > 0) ? this.getTempatPraktik().map( (data,index) => {
+            if(this.state.showAllLokasi === true){
+                return (
+                    <div className="profil__lokasi-praktik__item">
+                        <div className="profil__lokasi-praktik__item__rs">
+                            <p className="profil__lokasi-praktik__item__rs__nama">{data[0]['nama']}</p>
+                            <img className="profil__lokasi-praktik__item__rs__icon" src={Lokasi} alt="" />
+                            <p className="profil__lokasi-praktik__item__rs__alamat">{data[0]['alamat']}</p>
+                        </div>
+                        <img className="profil__lokasi-praktik__item__arrow" src={ArrowRight} alt="" />                      
+                    </div>
+                )
+            }else if(this.state.showAllLokasi === false){
+                if(index > 0){
+                    return (
+                        <div className="profil__lokasi-praktik__item-hide">
+                            <div className="profil__lokasi-praktik__item__rs">
+                                <p className="profil__lokasi-praktik__item__rs__nama">{data[0]['nama']}</p>
+                                <img className="profil__lokasi-praktik__item__rs__icon" src={Lokasi} alt="" />
+                                <p className="profil__lokasi-praktik__item__rs__alamat">{data[0]['alamat']}</p>
+                            </div>
+                            <img className="profil__lokasi-praktik__item__arrow" src={ArrowRight} alt="" />                      
+                        </div>
+                    ) 
+                }else{
+                    return (
+                        <div className="profil__lokasi-praktik__item">
+                            <div className="profil__lokasi-praktik__item__rs">
+                                <p className="profil__lokasi-praktik__item__rs__nama">{data[0]['nama']}</p>
+                                <img className="profil__lokasi-praktik__item__rs__icon" src={Lokasi} alt="" />
+                                <p className="profil__lokasi-praktik__item__rs__alamat">{data[0]['alamat']}</p>
+                            </div>
+                            <img className="profil__lokasi-praktik__item__arrow" src={ArrowRight} alt="" />                      
+                        </div>
+                    )
+                }
+            }
+            
+        }): '';
 
+        const jadwalList = (this.getTempatPraktik().length > 0) ? this.getTempatPraktik().map( (data, index) => {
+            
+            if(this.state.showAllJadwal === true){
+                return (
+                    <div className="profil__jadwal-praktik__item">
+                        <p className="profil__jadwal-praktik__rs">{data[0]['nama']}</p>
+                        <p className="profil__jadwal-praktik__date">
+                        {
+                            (d.getDay() == days.indexOf(data[1]['hari'])) ?
+                                d.getDate()+" "+month[d.getMonth()]+" "+d.getFullYear() :
+                                (Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) > monthDays[d.getMonth()]) ?
+                                Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) - monthDays[d.getMonth()] +" "+month[d.getMonth()+1]+" "+d.getFullYear() :
+                                Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) +" "+month[d.getMonth()]+" "+d.getFullYear()     
+                        }
+                        </p>
+                    <p className="profil__jadwal-praktik__jam">{data[1]['pukul']}</p>
+                    {
+                        (data[0]['janji_online'] == 'bisa') ? 
+                        <button className="profil__jadwal-praktik__btn">BUAT JANJI</button> :
+                        ''
+                    }
+                        
+                    </div>
+                )
+            }else if(this.state.showAllJadwal === false){
+                if(index > 0){
+                    return (
+                        <div className="profil__jadwal-praktik__item__hide">
+                            <p className="profil__jadwal-praktik__rs">{data[0]['nama']}</p>
+                            <p className="profil__jadwal-praktik__date">
+                            {
+                                (d.getDay() == days.indexOf(data[1]['hari'])) ?
+                                    d.getDate()+" "+month[d.getMonth()]+" "+d.getFullYear() :
+                                    (Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) > monthDays[d.getMonth()]) ?
+                                    Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) - monthDays[d.getMonth()] +" "+month[d.getMonth()+1]+" "+d.getFullYear() :
+                                    Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) +" "+month[d.getMonth()]+" "+d.getFullYear()     
+                            }
+                            </p>
+                        <p className="profil__jadwal-praktik__jam">{data[1]['pukul']}</p>
+                        {
+                            (data[0]['janji_online'] == 'bisa') ? 
+                            <button className="profil__jadwal-praktik__btn">BUAT JANJI</button> :
+                            ''
+                        }
+                            
+                        </div>
+                    )
+                }else{
+                    return (
+                        <div className="profil__jadwal-praktik__item">
+                            <p className="profil__jadwal-praktik__rs">{data[0]['nama']}</p>
+                            <p className="profil__jadwal-praktik__date">
+                            {
+                                (d.getDay() == days.indexOf(data[1]['hari'])) ?
+                                    d.getDate()+" "+month[d.getMonth()]+" "+d.getFullYear() :
+                                    (Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) > monthDays[d.getMonth()]) ?
+                                    Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) - monthDays[d.getMonth()] +" "+month[d.getMonth()+1]+" "+d.getFullYear() :
+                                    Number(d.getDate()) + Number(days.indexOf(data[1]['hari'])) +" "+month[d.getMonth()]+" "+d.getFullYear()     
+                            }
+                            </p>
+                        <p className="profil__jadwal-praktik__jam">{data[1]['pukul']}</p>
+                        {
+                            (data[0]['janji_online'] == 'bisa') ? 
+                            <button className="profil__jadwal-praktik__btn">BUAT JANJI</button> :
+                            ''
+                        }
+                            
+                        </div>
+                    )
+                }
+            }
+
+            
+        }): '';
+        
         const keahlianList = (this.props.selectedDokter.id !== undefined) ? this.props.selectedDokter.keahlian.map((data,index) => {
             if(this.state.showAllKeahlian === false){
                 if(index < 3){
@@ -112,28 +261,22 @@ export class ProfilDokter extends Component {
                 </div>
                 <p className="jadwal-praktik-terdekat">Jadwal Praktik Terdekat</p>
                 <div className="profil__jadwal-praktik">
-                    <div className="profil__jadwal-praktik__item">
-                        <p className="profil__jadwal-praktik__rs">Rumah Sakit Pusat Pertamina</p>
-                        <p className="profil__jadwal-praktik__date">Kamis, 10 Juni 2018</p>
-                        <p className="profil__jadwal-praktik__jam">10.00 - 12.00</p>
-                        <button className="profil__jadwal-praktik__btn">BUAT JANJI</button>
-                    </div>
+                    {
+                        jadwalList
+                    }
                     <div className="profil__jadwal-praktik__extra">
-                        <p className="profil__jadwal-praktik__extra__desc">Lihat Semua</p>
+                        <p className="profil__jadwal-praktik__extra__desc" onClick={this.showAllJadwal}>Lihat Semua</p>
                         <img className="profil__jadwal-praktik__extra__arrow" src={ArrowRight} alt=""/>
                     </div>                     
                 </div>
-                <p className="jadwal-praktik-terdekat">Lokasi Praktik</p>
+                <p className="profil__lokasi-praktik__title">Lokasi Praktik</p>
                 <div className="profil__lokasi-praktik">
-                    <div className="profil__jadwal-praktik__item">
-                        <p className="profil__jadwal-praktik__rs">Rumah Sakit Pusat Pertamina</p>
-                        <p className="profil__jadwal-praktik__date">Kamis, 10 Juni 2018</p>
-                        <p className="profil__jadwal-praktik__jam">10.00 - 12.00</p>
-                        <button className="profil__jadwal-praktik__btn">BUAT JANJI</button>
-                    </div>
-                    <div className="profil__jadwal-praktik__extra">
-                        <p className="profil__jadwal-praktik__extra__desc">Lihat Semua</p>
-                        <img className="profil__jadwal-praktik__extra__arrow" src={ArrowRight} alt=""/>
+                    {
+                        lokasiList
+                    }
+                    <div className="profil__lokasi-praktik__extra">
+                        <p className="profil__lokasi-praktik__extra__desc" onClick={this.showAllLokasi}>Lihat Semua</p>
+                        <img className="profil__lokasi-praktik__extra__arrow" src={ArrowRight} alt=""/>
                     </div>   
                 </div>
                 <p className="profil__keahlian__title">Keahlian</p>

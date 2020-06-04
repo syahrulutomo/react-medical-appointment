7c4006754fbd9bd02c30677913f64e18091456b7
syahrulutomo <irullvandalis@gmail.com>
2020-06-04 11:06:39 +0700

    [UPDATE] fix dependencies & fix indent

--- src/App.js
+++ src/App.js
@@ -9,23 +9,20 @@ import './App.scss';
 
 
 class App extends Component {
-  constructor(props){
-    super(props);
-  }
 
   render(){
     return (
-          <div className="app">
-            <Router>
-              <Switch>
-                <Route exact path="/" component={Home} />   
-                <Route path="/spesialisasi" component={SpesialisasiByLocation} />
-                <Route path="/jadwal" component={JadwalByLocation} />
-                <Route path="/profil" component={ProfilDokter} />
-                <Route path="/searched-profil" component={SearchedProfil} />
-              </Switch>
-            </Router>
-          </div>
+      <div className="app">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />   
+            <Route path="/spesialisasi" component={SpesialisasiByLocation} />
+            <Route path="/jadwal" component={JadwalByLocation} />
+            <Route path="/profil" component={ProfilDokter} />
+            <Route path="/searched-profil" component={SearchedProfil} />
+          </Switch>
+        </Router>
+      </div>
     )
   }
 }
